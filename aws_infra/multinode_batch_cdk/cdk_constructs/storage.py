be42527d04ed11adabd1c5aabb559c0c208a4b91
Wei Li <weiliw@amazon.com>
2022-09-16 23:29:59 +0000

    Add efs filesystem policy to avoid public access

--- aws_infra/multinode_batch_cdk/cdk_constructs/storage.py
+++ aws_infra/multinode_batch_cdk/cdk_constructs/storage.py
@@ -1,6 +1,7 @@
 from aws_cdk import aws_s3
 from aws_cdk import aws_efs
 from aws_cdk import aws_ec2
+from aws_cdk import aws_iam
 from aws_cdk import Stack
 from aws_cdk import RemovalPolicy
 
@@ -29,7 +30,7 @@ class PecosDistributedStorageStack(Stack):
         )
 
     def create_efs(self, vpc, security_group, user_name):
-        return aws_efs.FileSystem(
+        shared_efs = aws_efs.FileSystem(
             self,
             id="PECOS-Distributed-EFS",
             file_system_name=f"PECOS-Distributed-EFS-{user_name}",
@@ -43,3 +44,18 @@ class PecosDistributedStorageStack(Stack):
             performance_mode=aws_efs.PerformanceMode.GENERAL_PURPOSE,
             removal_policy=RemovalPolicy.DESTROY
         )
+        shared_efs.node.default_child.file_system_policy = aws_iam.PolicyDocument(
+            statements=[
+                aws_iam.PolicyStatement(
+                    effect=aws_iam.Effect.ALLOW,
+                    principals=[aws_iam.AnyPrincipal()],
+                    actions=[
+                        "elasticfilesystem:ClientMount",
+                        "elasticfilesystem:ClientWrite",
+                        "elasticfilesystem:ClientRootAccess",
+                    ],
+                    conditions={"Bool": {"elasticfilesystem:AccessedViaMountTarget": "true"}},
+                )
+            ]
+        )
+        return shared_efs
